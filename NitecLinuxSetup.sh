56831b301bdf51bd4709b3afb548cba90c0ca2a2
Peter Fleck <peterfleck@gmail.com>
2023-12-18 07:54:38 +0000

    Update NitecLinuxSetup.sh

--- NitecLinuxSetup.sh
+++ NitecLinuxSetup.sh
@@ -20,11 +20,10 @@ git config --global pull.rebase false
 
 #### SETUP VARIOUS MICROSOFT SOFTWARE ####
 
-# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-# sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-# rm -f packages.microsoft.gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+rm -f packages.microsoft.gpg
 
-# The above are now done together in the below command which installs az cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Get Ubuntu version
@@ -50,14 +49,14 @@ sudo apt-get --yes install dotnet-sdk-6.0 dotnet-sdk-7.0
 sudo apt-get --yes install intune-portal
 
 # INSTALL MICROSOFT EDGE
-
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo apt-get update && sudo apt-get --yes install microsoft-edge-stable
 
 
 # INSTALL VS CODE
-
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update && sudo apt-get --yes install code
 
 code --install-extension esbenp.prettier-vscode
@@ -81,8 +80,11 @@ sudo dpkg -i ~/Downloads/azuredatastudio.deb
 
 # INSTALL NODEJS LTS
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - &&\
-sudo apt-get install --yes nodejs
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
 
 # INSTALL MONGO COMPASS
 
@@ -99,7 +101,7 @@ sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 sudo apt update && sudo apt install --yes 1password
 
-# INSTALL NEOVIM && NvCHAD
+# INSTALL NEOVIM
 
 sudo apt-get --yes install ninja-build gettext cmake
 cd ~/Downloads
@@ -116,5 +118,7 @@ cd ~/Downloads
 sudo tar -xvf JetBrainsMono.tar.xz
 sudo cp -r *.ttf /usr/local/share/fonts
 sudo fc-cache -f -v
+cd ~/Downloads
+sudo rm -R *.ttf
 
 # DOWNLOAD DEVOPS REPOS HAS TO BE DONE IN SEPARATE SCRIPT AS NEED TO LOGIN TO COMPLIANT DEVICE
